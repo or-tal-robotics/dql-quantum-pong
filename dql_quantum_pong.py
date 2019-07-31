ff1442f1c8d6a8bfc5e45ef1f923bf27d06a6c2c
Or Tslil <ortslil64@gmail.com>
2019-07-31 07:14:29 +0300

    u

--- dql_quantum_pong.py
+++ dql_quantum_pong.py
@@ -39,7 +39,8 @@ def play_ones(env,
               epsilon_change,
               epsilon_min,
               pathOut,
-              record):
+              record,
+              train_idxs):
     
     t0 = datetime.now()
     obs = env.reset()
@@ -64,12 +65,15 @@ def play_ones(env,
         action = []
         for ii in range(2):
             action.append(model[ii].sample_action(state, epsilon))
+
         obs, reward, done, _ = env.step(action)
         obs_small = image_transformer.transform(obs, sess)
         next_state = update_state(state, obs_small)
         t0_2 = datetime.now()
+        
         for ii in range(2):
             episode_reward[ii] += reward[ii]
+        for ii in train_idxs:
             experience_replay_buffer[ii].add_experience(action[ii], obs_small, reward[ii], done)
             loss = learn(model[ii], target_model[ii], experience_replay_buffer[ii], gamma, batch_size)
         
@@ -110,7 +114,7 @@ if __name__ == '__main__':
     experience_replay_buffer = [ReplayMemory(),ReplayMemory()]
     episode_rewards = np.zeros((2,num_episodes))
     episode_lens = np.zeros(num_episodes)
-    
+    train_idxs = [0,1]
     epsilon = 1.0
     epsilon_min = 0.1
     epsilon_change = (epsilon - epsilon_min) / 500000
@@ -171,6 +175,13 @@ if __name__ == '__main__':
                 record = True
             else:
                 record = False
+                
+            if i%10 ==0:
+                if train_idxs == [0]:
+                    train_idxs = [1]
+                else:
+                    train_idxs = [0]
+                
             total_t, episode_reward, duration, num_steps_in_episode, time_per_step, epsilon = play_ones(
                     env,
                     sess,
@@ -185,13 +196,15 @@ if __name__ == '__main__':
                     epsilon_change,
                     epsilon_min,
                     video_path,
-                    record)
+                    record,
+                    train_idxs)
             for ii in range(2):
                 episode_rewards[ii,i] = episode_reward[ii]
             episode_lens[i] = num_steps_in_episode
             last_100_avg1 = episode_rewards[0,max(0,i-100):i+1].mean()
             last_100_avg2 = episode_rewards[1,max(0,i-100):i+1].mean()
             print("Episode:", i ,
+                  "training:", train_idxs, 
                   "Duration:", duration,
                   "Num steps:", num_steps_in_episode,
                   "Reward 1:", episode_reward[0],
@@ -204,7 +217,7 @@ if __name__ == '__main__':
         print("Total duration:", datetime.now()-t0)
         model.save()
         
-        y = smooth(episode_rewards)
+        y1 = smooth(episode_rewards)
         plt.plot(episode_rewards, label='orig')
         plt.plot(y, label='smoothed')
         plt.legend()
